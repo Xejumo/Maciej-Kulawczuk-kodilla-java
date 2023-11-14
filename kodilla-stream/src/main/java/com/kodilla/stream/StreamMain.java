a385ca40d11c8c5678502244ad6e55aa7944db8b
Maciej <kulawczuk.maciek@gmail.com>
2023-11-14 23:58:21 +0100

    7.2 Czym jest Stream?

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
@@ -18,5 +19,8 @@ public class StreamMain {
         System.out.println(beautifier.beautify("This is a test.", decorator2));
         System.out.println(beautifier.beautify("Stream processing", decorator3));
         System.out.println(beautifier.beautify("Replace all 'a' with '@'", decorator4));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
