d6ae276c6cd876295170ee7b1e5d97576bf61ad0
Maciej <kulawczuk.maciek@gmail.com>
2023-11-14 23:32:14 +0100

    7.1 Wstęp do programowania funkcyjnego, czyli wyrażenia lambda

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,22 @@
+package com.kodilla.stream;
+
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        PoemBeautifier beautifier = new PoemBeautifier();
+
+        PoemDecorator decorator1 = text -> "ABC" + text + "ABC";
+        PoemDecorator decorator2 = String::toUpperCase;
+        PoemDecorator decorator3 = text -> ">>>" + text + "<<<";
+        PoemDecorator decorator4 = text -> text.replace("a", "@");
+
+        System.out.println(beautifier.beautify("Hello, World!", decorator1));
+        System.out.println(beautifier.beautify("This is a test.", decorator2));
+        System.out.println(beautifier.beautify("Stream processing", decorator3));
+        System.out.println(beautifier.beautify("Replace all 'a' with '@'", decorator4));
+    }
+}
---
+++ kodilla-stream/src/test/java/com/example/kodillastream/KodillaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
